1f0b3954341e612f5b832902efa6584d3ce00b67
Elisabeth Appel <73082739+elisabethappel@users.noreply.github.com>
2021-03-18 20:00:54 -0400

    reformat sidebar, legend

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ var map = new mapboxgl.Map({
 
 var nav = new mapboxgl.NavigationControl();
 
-map.addControl(nav, 'top-right');
+map.addControl(nav, 'top-left');
 
 
 map.on('style.load', function () {
@@ -17,37 +17,30 @@ map.on('style.load', function () {
   // we need to load the data manually because our population properties are strings and not numbers!!!
 
   //load legend on map load
-  var layers = ['0-55', '55-70', '70-85', '85-100'];
-  var colors = ['red', 'orange', 'yellow', 'green'];
+//  var layers = ['0-55', '55-70', '70-85', '85-100'];
+//  var colors = ['red', 'orange', 'yellow', 'green'];
 
-  for (i = 0; i < layers.length; i++) {
-    var layer = layers[i];
-    var color = colors[i];
-    var item = document.createElement('div');
-    var key = document.createElement('span');
-    key.className = 'legend-key';
-    key.style.backgroundColor = color;
+//  for (i = 0; i < layers.length; i++) {
+//    var layer = layers[i];
+//    var color = colors[i];
+//    var item = document.createElement('div');
+//    var key = document.createElement('span');
+//    key.className = 'legend-key';
+//    key.style.backgroundColor = color;
 
-    var value = document.createElement('span');
-    value.innerHTML = layer;
-    item.appendChild(key);
-    item.appendChild(value);
-    legend.appendChild(item);
-}
+//    var value = document.createElement('span');
+//    value.innerHTML = layer;
+//    item.appendChild(key);
+//    item.appendChild(value);
+//    legend.appendChild(item);
+//}
 
  $.getJSON('data/ll84_energy_map.geojson', function(featureCollection) {
 
-   // iterate over each feature in the FeatureCollection and convert the pop2010 property to a number
    featureCollection.features.forEach(function(feature) {
      feature.properties.disclosure_pluto_es_score = parseInt(feature.properties.disclosure_pluto_es_score)
    })
 
-   console.log(featureCollection)
-   console.log('hello')
-
-   //console.log('style loaded')
-   // override the fill color of the water layer
-   //map.setPaintProperty('water', 'fill-color', '#c9f4ff');
 
    // add a geojson source
    map.addSource('nyc-bbls', {
@@ -85,16 +78,34 @@ map.on('style.load', function () {
  })
 })
 
-map.on('mousemove', function(e) {
+
+
+map.on('click', function(e) {
   var nycbbls = map.queryRenderedFeatures(e.point, {
     layers: ['nycbbls']
   });
 
   if (nycbbls.length > 0) {
-    document.getElementById('pd').innerHTML = '<h3><strong>' + nycbbls[0].properties.score + '</strong></h3><p><strong><em>';
-    map.getCanvas().style.cursor = 'pointer';
+    document.getElementById('score').innerHTML = '<h4 style = "font-size:300%" ><strong>' + nycbbls[0].properties.score + '</strong></h4><p><strong><em>';
   } else {
-    document.getElementById('pd').innerHTML = '';
-    map.getCanvas().style.cursor = '';
+    document.getElementById('score').innerHTML = '';
   }
 });
+
+map.on('mousemove', function(d) {
+  var nycbbls = map.queryRenderedFeatures(d.point, {
+    layers: ['nycbbls']
+  });
+  if (nycbbls.length > 0) {
+    map.getCanvas().style.cursor = 'pointer';
+  } else {
+    map.getCanvas().style.cursor = '';
+}
+});
+
+map.addControl(
+new MapboxGeocoder({
+accessToken: mapboxgl.accessToken,
+mapboxgl: mapboxgl
+})
+);
